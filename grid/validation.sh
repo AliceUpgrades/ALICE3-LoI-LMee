cb8f14f577d43a29d46a430666ee7dc7b515c3ef
Sebastian Scheid <s.scheid@cern.ch>
2021-06-03 12:50:00 +0200

    Check that output file is not empty

--- grid/validation.sh
+++ grid/validation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ -s *.root ]; then #check that we have a non empty root file.
+if [ -s output.root ]; then #check that we have a non empty root file.
   exit 0;
 else
   exit 1;
